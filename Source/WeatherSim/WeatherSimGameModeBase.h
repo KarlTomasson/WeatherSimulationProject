ac4b37fa43dea52cda38dbfd9624b49e94925d7a
KarlTomasson <karltomasson3@gmail.com>
2025-01-07 21:27:40 +0200

    Copy of the previous repository
    Copied the contents of the previous repository

---
+++ Source/WeatherSim/WeatherSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WeatherSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WEATHERSIM_API AWeatherSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
